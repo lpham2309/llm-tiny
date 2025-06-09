5f84655c64f0a19e8db52e813d9c51bf487cdd0b
Lam Pham <clbt.pham3110@gmail.com>
2025-06-08 23:22:44 -0400

    WIP adding new key to support new vocab

--- SimpleTokenizerV1.py
+++ SimpleTokenizerV1.py
@@ -1,3 +1,20 @@
+import re
 class SimpleTokenizerV1:
     def __init__(self, vocab):
-     pass
+     self.str_to_int = vocab
+     self.int_to_str = {i:s for s, i in vocab.items()}
+
+    def encode(self, text):
+        pre_processed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        pre_processed = [
+            item.strip() for item in pre_processed if item.strip()
+        ]
+
+        ids = [self.str_to_int[s] for s in pre_processed]
+        return ids
+
+
+    def decode(self, token_ids):
+        text = " ".join([self.int_to_str[id] for id in token_ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
